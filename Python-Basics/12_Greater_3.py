6b5e1ecee611f157ffe19fab56e2a733e1b39067
Md Asif <asif212327@gmail.com>
2026-01-19 00:47:52 +0530

    Add files via upload

---
+++ Python-Basics/12_Greater_3.py
@@ -0,0 +1,4 @@
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+c=int(input("Enter third number: "))
+print("The greatest number is:", a if a>b and a>c else b if b>c else c)
