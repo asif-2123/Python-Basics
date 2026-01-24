c2a8e734332ab716ba4699cf5970f2e6eba086b2
Md Asif <asif212327@gmail.com>
2026-01-25 01:29:00 +0530

    Add files via upload

---
+++ Python-Basics/22_Check_input.py
@@ -0,0 +1,7 @@
+ch=input("Enter the character: ")
+if ch.isalpha():
+    print("The answer is: Alphabet")
+elif ch.isdigit():
+    print("The answer is: Digit")
+else:
+    print("The answer is: Special Character")
---
+++ Python-Basics/23_Case.py
@@ -0,0 +1,5 @@
+ch=input("Enter the character: ")
+if ch.isupper():
+    print("The answer is: Uppercase")
+elif ch.islower():
+    print("The answer is: Lowercase")
