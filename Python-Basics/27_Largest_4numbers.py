511bec67f52ed88bd07266365ff60da58f004d08
Md Asif <asif212327@gmail.com>
2026-01-27 00:13:29 +0530

    Add files via upload

---
+++ Python-Basics/27_Largest_4numbers.py
@@ -0,0 +1,9 @@
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+c=int(input("Enter third number: "))
+d=int(input("Enter fourth number: "))
+max=a
+if b>max: max=b
+if c>max: max=c
+if d>max: max=d
+print("The answer is:",max)
