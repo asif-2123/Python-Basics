d12259740106d7891cc8d4e88282a2c2a7363285
Md Asif <asif212327@gmail.com>
2026-01-28 07:14:46 +0530

    Add files via upload

---
+++ Python-Basics/29_Smallest_4numbers.py
@@ -0,0 +1,9 @@
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+c=int(input("Enter third number: "))
+d=int(input("Enter fourth number: "))
+min=a
+if b<min: min=b
+if c<min: min=c
+if d<min: min=d
+print("The answer is:",min)
