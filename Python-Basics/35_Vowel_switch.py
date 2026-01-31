ca56cb361b112584db481bc12653152d2f4f0e5e
Md Asif <asif212327@gmail.com>
2026-01-31 10:04:24 +0530

    Add files via upload

---
+++ Python-Basics/35_Vowel_switch.py
@@ -0,0 +1,5 @@
+ch=input("Enter the character: ")
+if ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u' or ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U':
+    print("The answer is: Vowel")
+else:
+    print("The answer is: Not a Vowel")
