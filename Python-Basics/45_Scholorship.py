cb450bd0bf2419c5b9aa26407dd018d6cdc1cdf8
Md Asif <asif212327@gmail.com>
2026-02-04 00:39:15 +0530

    Add files via upload

---
+++ Python-Basics/45_Scholorship.py
@@ -0,0 +1,6 @@
+avg=float(input("Enter average marks: "))
+income=float(input("Enter family income: "))
+if avg>=75 and income<=200000:
+    print("Scholarship Approved")
+else:
+    print("Scholarship Not Approved")
